61f2fb040728607ada67e485419d9d33ac0a0f5b
Ravikant <kant81272@gmail.com>
2024-01-19 22:54:15 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://deploy-mern-1whq.vercel.app"],
+    origin: ["https://note-vault-front.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
