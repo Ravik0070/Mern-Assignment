03964445a16f747401f8159bb8c8956a931dd1c8
Ravikant <kant81272@gmail.com>
2024-01-20 00:37:44 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["https://note-vault-client.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
